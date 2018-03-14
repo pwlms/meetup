b7d7de74db2d69cf8e83d541964453704710e61c
Ahmed Shaaban <ahmed.vuw@gmail.com>
2018-03-14 17:06:55 +0300

    app is running locally

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgsn = require('morgan');
+const app = express();
+
+// App Setup
+
+
+
+// Server Setup
+const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+server.listen(port);
+console.log(`Server listening on port ${port}`);
