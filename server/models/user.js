3fa70f67358ba206b2e4d8c39181632dc0b2f4a2
Ahmed Shaaban <ahmed.vuw@gmail.com>
2018-03-15 13:09:19 +0300

    auth is working

--- server/models/user.js
+++ server/models/user.js
@@ -12,19 +12,27 @@ const userSchema = new Schema({
 });
 
 userSchema.pre('save', function (next) {
- const user = this;
+ const self = this;
 
  bcrypt.genSalt(10, function (err, salt) {
   if (err) return next(err);
 
-  bcrypt.hash(user.password, salt, null, function (err, hash) { 
+  bcrypt.hash(self.password, salt, null, function (err, hash) {
    if (err) return next(err);
 
-   user.password = hash;
+   self.password = hash;
    next();
    })
  });
-})
+});
+
+userSchema.methods.comparePassword = function (candidatePassword, callback) {
+  bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+    if (err) return callback(err);
+
+    callback(null, isMatch);
+   });
+}
 
 const UserModel = mongoose.model('user', userSchema);
 
